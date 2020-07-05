db76d9a8d42d3d025d6c0e8a561ac3f4da143839
Amru Rosyada <amru.rosyada@gmail.com>
2020-07-06 00:50:27 +0700

    update depedency on web template

--- nakefile.nim
+++ nakefile.nim
@@ -315,7 +315,6 @@ proc installDeps(appName: string) =
     
     elif pkgCmd.startsWith("develop"):
       let (output, errorCode) = @["cd", nimdepsDir, "&&", "nimble", "-y", pkgCmd].join(" ").execCmdEx
-      echo output
       if errorCode != 0:
         for err in output.split("\n"):
           let errMsg = err.strip
