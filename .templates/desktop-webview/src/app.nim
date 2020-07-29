b0ac67798eea729cbcfdd46da8f537d55f74af2b
Amru Rosyada <amru.rosyada@gmail.com>
2020-07-29 12:17:02 +0700

    fix template for webview desktop

--- .templates/desktop-webview/src/app.nim
+++ .templates/desktop-webview/src/app.nim
@@ -1,8 +1,14 @@
 import distros, os
 
-if detectOs(Windows):
-  discard execShellCmd("start" & getAppDir().joinPath("appName_srv.exe"))
-  discard execShellCmd("start " & getAppDir().joinPath("appName_wv.exe"))
+when defined release:
+  if detectOs(Windows):
+    discard execShellCmd("start" & getAppDir().joinPath("appName_srv.exe"))
+    discard execShellCmd("start " & getAppDir().joinPath("appName_wv.exe"))
+  else:
+    discard execShellCmd(getAppDir().joinPath("appName_srv &"))
+    discard execShellCmd(getAppDir().joinPath("appName_wv &"))
 else:
-  discard execShellCmd(getAppDir().joinPath("appName_srv &"))
-  discard execShellCmd(getAppDir().joinPath("appName_wv &"))
+  if detectOs(Windows):
+    discard execShellCmd(getAppDir().joinPath("appName_srv.exe"))
+  else:
+    discard execShellCmd(getAppDir().joinPath("appName_srv"))
