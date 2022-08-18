bbb6e1b2d29f4591bc0fff5ef656d8025a281069
Amru Rosyada <dev@zendbit.local.zendbit.dev>
2022-08-18 09:35:06 +0700

    auto reload page on before route

--- .templates/web/src/backend/home.nim
+++ .templates/web/src/backend/home.nim
@@ -3,9 +3,11 @@ import zfcore/server
 import zfplugs/layout
 
 var page {.threadvar.}: Layout
-page = newLayoutFromFile("home.mustache")
 
 routes:
+  before:
+    page = newLayoutFromFile("home.mustache")
+
   # accept request with /example/123456
   # id will capture the value 12345
   get "/index/<id>":
--- .templates/web/src/backend/index.nim
+++ .templates/web/src/backend/index.nim
@@ -3,12 +3,12 @@ import zfcore/server
 import zfplugs/layout
 
 var page {.threadvar.}: Layout
-page = newLayoutFromFile("index.mustache")
 
 routes:
   # accept request with /example/123456
   # id will capture the value 12345
   before:
+    page = newLayoutFromFile("index.mustache")
     page.c["siteUrl"] = siteUrl
     page.c["appVersion"] = getAppFilename().extractFilename
 
