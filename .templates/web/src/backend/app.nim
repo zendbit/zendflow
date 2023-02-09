801d93fe880eb1eab40b65dcb1e2070a4bfa42f0
Zendbit Solution <zendbit@slackware.zendbit.local>
2023-02-09 14:35:56 +0700

    change default config

--- .templates/web/src/backend/app.nim
+++ .templates/web/src/backend/app.nim
@@ -10,13 +10,29 @@
 ## import zfcore
 import zfcore/server
 #import example
+import zfplugs/settings
 import index
+
+##  shared settings as threadvar
+var jSettings {.threadvar.}: JsonNode
 
 routes:
   staticDir "/"
 
+  before:
+    ##  set settings value before routing
+    jSettings = jsonSettings()
+
   after:
-    response.headers["Access-Control-Allow-Origin"] = "http://localhost:9001"
-    response.headers["Access-Control-Allow-Credentials"] = "true"
+    ##  check if settings is exists
+    if not jSettings.isNil:
+      ##  check if current request if from allowed origin
+      ##  check againts allowedOrigin list from settings.json
+      let allowedOrigin = jSettings{"allowedOrigin"}
+      if not allowedOrigin.isNil and
+        request.url.getDomain in allowedOrigin.to(seq[string]):
+        ##  set the allowr origin header to allow current domain request
+          response.headers["Access-Control-Allow-Origin"] = request.url.getDomain
+          response.headers["Access-Control-Allow-Credentials"] = "true"
 
 emitServer()
