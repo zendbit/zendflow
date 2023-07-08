326f9f0a5c5bdf4450cee04ca73b454021d36e51
Zendbit Solution <zendbit@LinuxBOX.zendbit.local>
2023-07-08 16:52:01 +0700

    remove unused code

--- .templates/web/src/backend/index.nim
+++ .templates/web/src/backend/index.nim
@@ -4,12 +4,6 @@ import zfplugs/zview
 
 var page {.threadvar.}: ZView
 
-##  check if manifest.json exists in www
-var manifest {.threadvar.}: JsonNode
-let manifestFile = "wwww".joinPath("manifest.json")
-if manifestFile.fileExists:
-    manifest = parseFile(manifestFile)
-
 routes:
   # accept request with /example/123456
   # id will capture the value 12345
@@ -21,5 +15,8 @@ routes:
     if manifest.isNil:
       page.c["appName"] = manifest{"name"}.getStr
 
-  get "/index.html":
+  get "/":
+    respRedirect("/index")
+
+  get "/index":
     Http200.respHtml(page.render())
